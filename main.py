acc01e57801e34be5102acd9d2940a525933b186
richwhoispoor <36944428+richwhoispoor@users.noreply.github.com>
2018-03-01 15:33:47 +1100

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+# welcome to the adventure game
+
+# import random routine
+import random
+
+# define sub routines
+# inspect decision for result
+def decision(UserDecision):
+     userdecision = input()
+     
+     
+
+print('Welcome pilgrim, this is your wild west adventure game')
+print(' ')
+print('The game is easy to navigate and play')
+print('follow the instructions to determine your next action')
+print('the results of your action will be displayed on your screen')
+print('after you have enter your decision')
+print(' ')
+print('Think wisely for each action')
+print(' ')
+# define character, horse name and country name
+print('Lets start by typing your name?')
+CharactorName = input()
+print('Well hello there ' + CharactorName)
+print('Nice to meet you')
+print(' ')
+print('Now can you type your horses name?')
+HorseName = input()
+print('What a might steed you are ' + HorseName)
+print(' ')
+print(' Can you type the name of the place that ' + CharactorName + ' lives?')
+CountryName = input()
+print(CountryName + ' sounds like a strange place!')
+print(' ')
+print(' ')
+print(' ')
+print('Lets get started')
+print(' ')
+print(' ')
+print(' ')
+# start the adventure
+# 
+print('It is a dark and stormy day in ' + CountryName)
+print('The rain is very heavy and ' + CharactorName + ' is drenched')
+print(HorseName + '
