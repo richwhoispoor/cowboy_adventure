391c296128d296272874cde0e91960f7dc7fe4ee
richwhoispoor <36944428+richwhoispoor@users.noreply.github.com>
2018-05-01 21:43:01 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -5,9 +5,9 @@
 
 # define sub routines
 # inspect decision for result
-def decision(UserDecision):
-     userdecision = input()
-     
+#def decision(UserDecision):
+#     userdecision = input()
+#H     
      
 print(' ')
 print('Welcome pilgrim, this is your wild west adventure game!!!!')
@@ -29,7 +29,8 @@ CharaterGender = input()
 #if CharaterGender == 'male' or 'female':
 #    break
 #else
-#   print('Please use only male or female':
+#   print('Please use only male or female')
+#
 print(' ')
 print('Now can you type your horses name?')
 HorseName = input()
@@ -41,7 +42,7 @@ print(CountryName + ' sounds like a strange place!')
 print(' ')
 print('Lets get started')
 print(' ')
-
+#
 # start the adventure
 # 
 print('It is a dark and stormy day in ' + CountryName)
@@ -49,11 +50,28 @@ print('The rain is very heavy and ' + CharactorName + ' is drenched.')
 print(' ')
 print(CharactorName + ' sees a dry cave ahead.  Type "1" if you "go in" or type "2" if you "keep going"?')
 UserDecision = input()
-try UserDecision == 1:
-    print('Ok')
-elif UserDecision == 2:
-    Print('you get dry')
-    
+#
+# cave section
+#
+if UserDecision == '1':
+    print('You and' + HorseName + ' walk into the cave.  You immediatly feel better as you go in the cave.')
+    print('After dismounting, you take off your outer clothes.')
+    print(' ')
+    print('You decide to start and fire to warm up and dry off your clothes.  You gather some sticks from within the cave.')
+    print('As you move to the back of the cave you feel a breeze, but the priority is to get warm, so you keep gathering sticks.')
+    print(' ')
+    print('There is soon a medium sized fire going in the cave.  After positioning your clothes on some stick to dry out you are')
+    print('able to attend to' + HorseName + 'by taking off the saddle and brushing him down.')
+    Print(HorseName + 'really appreciates the attention and swings his body over to you to keep warm.')
+    print(' ')
+    print('you finally start to relax and enjoy the cave, as you throw on more wood to get thinking back to the breeze at the back')
+    print('of the cave.  After a few minutes you thrown more wood on the fire to keep it burning, and select a good sized stick to')
+    print('use as a fire touch.  The fire throws interesting shapes off the walls of the cave as you walk towards the back wall.')
+    print(' ')
+    print('The flame from the torch blows size ways as you get close to the back.  As you lower the torch to the back wall the flame')
+    print('changes direction.  You look around a see a crack that runs from the rook to the floor.  It looks to be about body size')
+    print('')
 
+elif expression:
+    Print('you keep going in the rain')
 
-#print(HorseName + '
